a37b0b8c18c989aa55df2cdb318228b767bfdae1
mh22lee <martin.mh.lee@gmail.com>
2016-01-30 06:58:07 -0800

    Start Page Added

---
+++ ggj16/Assets/StartButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	public void moveScene()
+    {
+        Global.level = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
