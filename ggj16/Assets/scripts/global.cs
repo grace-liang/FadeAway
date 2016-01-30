2d7afc516647dd4454929fdd8525b63f9fead5ea
mh22lee <martin.mh.lee@gmail.com>
2016-01-30 01:24:26 -0800

    Fix timer level

--- ggj16/Assets/scripts/global.cs
+++ ggj16/Assets/scripts/global.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class Global
 {
-    public static int level = 1;
+    public static int level = SceneManager.GetActiveScene().buildIndex + 1;
     public static float[] times = new float[5] { 15.0f, 20.0f, 20.0f, 25.0f, 30.0f };
 }
