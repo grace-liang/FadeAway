fa9253779b21be3dcf5a21f88cabf1e9a31bd6cb
Grace Liang <acie.liang@gmail.com>
2016-01-29 22:20:36 -0800

    Walls disappear after 2 seconds

--- ggj16/Assets/scripts/Wall.cs
+++ ggj16/Assets/scripts/Wall.cs
@@ -4,17 +4,23 @@ using System.Collections;
 public class Wall : MonoBehaviour {
 
     public Renderer rend;
+	private bool wallsHidden;
 
     // Use this for initialization
     void Start () {
+		wallsHidden = false;
         rend = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!wallsHidden && Time.fixedTime > 2) {
+			this.GetComponent<SpriteRenderer>().color = Color.white;
+		}
+
         if (Input.GetKey(KeyCode.Space))
         {
-            this.GetComponent<SpriteRenderer>().color = Color.white;
+            this.GetComponent<SpriteRenderer>().color = Color.red;
         }
     }
 
