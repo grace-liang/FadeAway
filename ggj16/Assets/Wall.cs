c0c52216a46201c1ed715fc08b56dfb184172724
mh22lee <martin.mh.lee@gmail.com>
2016-01-29 22:04:27 -0800

    Spacebar event

--- ggj16/Assets/Wall.cs
+++ ggj16/Assets/Wall.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class Wall : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.GetComponent<SpriteRenderer>().color = Color.white;
+        }
+    }
 
 	void OnTriggerEnter2D (Collider2D other) {
 		Debug.Log ("COLLISION");
