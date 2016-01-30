6997685038dcddfa5273252f40e77e41a0ba2469
mh22lee <martin.mh.lee@gmail.com>
2016-01-29 22:05:31 -0800

    Fix Space

--- ggj16/Assets/Wall.cs
+++ ggj16/Assets/Wall.cs
@@ -3,11 +3,8 @@ using System.Collections;
 
 public class Wall : MonoBehaviour {
 
-    public Renderer rend;
-
     // Use this for initialization
     void Start () {
-        rend = GetComponent<Renderer>();
 	}
 	
 	// Update is called once per frame
