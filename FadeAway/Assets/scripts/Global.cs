533edf565b573f38e9de48fdf468f54691296522
Martin <m4rtin.t@gmail.com>
2016-01-31 00:17:25 -0800

    Changning global variables

--- FadeAway/Assets/scripts/Global.cs
+++ FadeAway/Assets/scripts/Global.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class Global
 {
 
+
 	// The position of the current level in the build index.
 	// + 1 if including Start Menu.
 	public static int level = SceneManager.GetActiveScene ().buildIndex + 1;
@@ -12,4 +13,35 @@ public class Global
 	// Available alloted for each level.
 	// First one is for the start menu.
 	public static float[] times = new float[6] { 0.0f, 5.0f, 10.0f, 15.0f, 20.0f, 20.0f };
+
+	//Total Power of player
+	protected double power = 100;
+
+
+	public double GetPower ()
+	{
+		return power;
+	}
+
+	public void AddPower (double number)
+	{
+		double newPower;
+		newPower = power += number;
+		if (newPower > 100) {
+			power = 100;
+		} else {
+			power = newPower;
+		}
+	}
+
+	public void MinusPower (double number)
+	{
+		double newPower;
+		newPower = power -= number;
+		if (newPower < 0) {
+			power = 0;
+		} else {
+			power = newPower;
+		}
+	}
 }
