570bee2c6159022c341d093bf248a793fad50a7a
mh22lee <martin.mh.lee@gmail.com>
2016-01-30 19:36:32 -0800

    Build Settings Updated

--- FadeAway/Assets/scripts/Global.cs
+++ FadeAway/Assets/scripts/Global.cs
@@ -7,7 +7,7 @@ public class Global
 
 	// The position of the current level in the build index.
 	// + 1 if including Start Menu.
-	public static int level = SceneManager.GetActiveScene ().buildIndex;
+	public static int level = SceneManager.GetActiveScene ().buildIndex + 1;
 
 	// Available alloted for each level.
 	public static float[] times = new float[5] { 15.0f, 20.0f, 20.0f, 25.0f, 30.0f };
