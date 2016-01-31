1a28741e9c8d35cab4b7af174f470f54ef0c3f97
Martin <m4rtin.t@gmail.com>
2016-01-31 11:30:24 -0800

    Power Up Sound Effect Fix

--- FadeAway/Assets/scripts/Charge.cs
+++ FadeAway/Assets/scripts/Charge.cs
@@ -4,21 +4,29 @@ using System.Collections;
 public class Charge : MonoBehaviour
 {
 	private Wall[] walls;
-
+	public AudioClip powerup_sound;
 
 	void Start ()
 	{
 		walls = FindObjectsOfType (typeof(Wall)) as Wall[];
+
 	}
+
 
 	void OnTriggerEnter2D (Collider2D other)
 	{
 		Debug.Log ("Player collided with a charge.");
+		// Play sound effect
+		AudioSource source = GetComponent<AudioSource>();
+		source.PlayOneShot (powerup_sound, 0.7F);
 
 		foreach (Wall w in walls) {
 			w.AddDuration ();
 		}
 
-		Destroy (gameObject);
+		Destroy (this.GetComponent<Collider2D>());
+		Destroy (this.GetComponent<Animator>());
+		Destroy (this.GetComponent<SpriteRenderer> ());
+	
 	}
 }
