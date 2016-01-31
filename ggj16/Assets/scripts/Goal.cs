ca70efeac81bff70c5eeb1787587893a5d5b1501
Christopher Yang <chris.ct.yang@gmail.com>
2016-01-30 16:17:14 -0800

    Remove start menu

---
+++ ggj16/Assets/scripts/Goal.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class goal : MonoBehaviour {
+
+	private player_movement player;
+
+	// Use this for initialization
+	void Start () {
+		player = FindObjectOfType (typeof(player_movement)) as player_movement;
+	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+	//	death ();
+		player.add_power(25);
+		Debug.Log ("Power level: "+player.get_power ());
+        Global.level += 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+	}
+		
+}
